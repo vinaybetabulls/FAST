9b9ac2fd53ed18220d9d6dab7bcb1202ae460f36
tmprakash3 <tmprakash3@users.noreply.github.com>
2021-06-11 09:11:18 +0530

    Made category text ellipsis and scrollbar when text increase

--- renderer/components/Main/MiteBodyContainer/MiteBodyContainer.tsx
+++ renderer/components/Main/MiteBodyContainer/MiteBodyContainer.tsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import './MiteBodyContainer.styles.css'
+import { Empty } from 'antd';
 const MiteBodyContainer = () => {
     return (
         <div className="miteBodyConainer">
             <div className="mite-nav-title">
                 <h3>MITE Project</h3>
+            </div>
+            <div className="align-center">
+            <Empty />
             </div>
         </div>
     )
