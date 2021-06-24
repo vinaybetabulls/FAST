6652e0b1bc3720b438fb859dea4d4d64dcae28f2
vbodige <vinay.bodige@interflora.co.uk>
2021-06-24 23:31:28 +0530

    adding tab view

--- renderer/components/IconHeader/IconHeader.tsx
+++ renderer/components/IconHeader/IconHeader.tsx
@@ -45,6 +45,7 @@ const IconHeader = () => {
         {/* <UndoOutlined style={{color:"", fontSize: "18px"}} />
       <RedoOutlined style={{color:"", fontSize: "18px"}} /> */}
         <ExportOutlined style={{ color: "#00a8f5", fontSize: "18px" }} />
+        <Image src="/svgs/clean.svg" height="25" width="25"/>
       </div>
       <div className="testCaseOptions">
         <Select
--- renderer/pages/home.tsx
+++ renderer/pages/home.tsx
@@ -4,22 +4,26 @@ import IconHeader from "../components/IconHeader/IconHeader";
 import SideNav from "../components/SideNav/SideNav";
 import MiteBodyContainer from "../components/Main/MiteBodyContainer/MiteBodyContainer";
 import { default as MiteFooter } from "../components/Footer/Footer";
+import TabView from "../components/TabView/TabView";
 
 const { Header, Content, Footer } = Layout;
+import { AppProvider } from "../Context/MainContext";
 
 function Home() {
   return (
     <React.Fragment>
-      <Header className="icon-header">
-        <IconHeader />
-      </Header>
-      <Content style={{ display: "flex" }}>
-        <SideNav />
-        <MiteBodyContainer />
-      </Content>
-      <Footer>
-        <MiteFooter />
-      </Footer>
+      <AppProvider>
+        <Header className="icon-header">
+          <IconHeader />
+        </Header>
+        <Content style={{ display: "flex" }}>
+          <SideNav />
+          <TabView />
+        </Content>
+        <Footer>
+          <MiteFooter />
+        </Footer>
+      </AppProvider>
     </React.Fragment>
   );
 }
