8ab031c94a7fdc71ac6268b728ca3446423c000e
vbodige <vinay.bodige@interflora.co.uk>
2021-06-29 13:54:53 +0530

    context menu for tab right click

--- renderer/components/MenuCard/CloseTabMenu.tsx
+++ renderer/components/MenuCard/CloseTabMenu.tsx
@@ -4,19 +4,25 @@ import { Menu } from "antd";
 type Props = {
   top: number;
   left: number;
-  handleTestSuiteModal: () => void
+  closeOtherTabs: () => void;
+  onClose: () => void;
+  onCloseAllTabs: ()=>void
 };
 const CloseTabMenu = (props: Props) => {
-  const { top, left, handleTestSuiteModal } = props;
+  const { top, left, onCloseAllTabs, onClose, closeOtherTabs } = props;
   return (
     <Menu style={{ position: "absolute", top: top, left: left, zIndex: 999 }}>
       <Menu.Item key="1">
-        <span className="menu-text">Close</span>
+        <span className="menu-text" onClick={onClose}>
+          Close
+        </span>
       </Menu.Item>
       <Menu.Item key="2">
-        <span className="menu-text" onClick={handleTestSuiteModal}>Close all</span>
+        <span className="menu-text" onClick={onCloseAllTabs}>
+          Close all
+        </span>
       </Menu.Item>
-      <Menu.Item key="3">
+      <Menu.Item key="3" onClick={closeOtherTabs}>
         <span className="menu-text">Close others</span>
       </Menu.Item>
     </Menu>
